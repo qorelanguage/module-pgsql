e6cf2e46a867df50295002acdd374f42ee480f3e
David Nichols <david.nichols@qoretechnologies.com>
2021-05-25 09:26:12 +0200

    fixed checking tests

--- test/docker_test/test-ubuntu.sh
+++ test/docker_test/test-ubuntu.sh
@@ -45,4 +45,13 @@ export QORE_MODULE_DIR=${MODULE_SRC_DIR}/qlib:${QORE_MODULE_DIR}
 cd ${MODULE_SRC_DIR}
 for test in test/*.qtest; do
     gosu qore:qore qore $test -vv
+    RESULTS="$RESULTS $?"
+    date
+done
+
+# check the results
+for R in $RESULTS; do
+    if [ "$R" != "0" ]; then
+        exit 1 # fail
+    fi
 done
